c3787163083c3aa725887cddb0c7840807b5bfb0
bryanz826 <bryanz826@gmail.com>
2021-03-17 01:21:53 -0700

    Add TypeScript, ThreeJS Support
    - threejs compatibilities with react & nextjs
    - threejs tutorial simple at /threejs-tutorial

---
+++ pages/threejs-tutorial.tsx
@@ -0,0 +1,46 @@
+import React, { useRef, useState } from "react";
+import { Canvas, useFrame } from "react-three-fiber";
+import { Mesh } from "three";
+import styles from "../styles/ThreeJsTutorial.module.scss";
+
+const Box = (props) => {
+  const mesh = useRef<Mesh>();
+
+  const [hovered, setHover] = useState(false);
+  const [active, setActive] = useState(false);
+
+  useFrame(() => {
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
+  });
+
+  return (
+    <mesh
+      {...props}
+      ref={mesh}
+      scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+      onClick={() => setActive(!active)}
+      onPointerOver={() => setHover(true)}
+      onPointerOut={() => setHover(false)}
+    >
+      <boxBufferGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
+    </mesh>
+  );
+};
+
+const ThreeJsTutorial = () => {
+  return (
+    <Canvas className={styles.container}>
+      <ambientLight intensity={0.5} />
+      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
+      <pointLight position={[-10, -10, -10]} />
+      <Box position={[-1.2, 0, 0]} />
+      <Box position={[1.2, 0, 0]} />
+    </Canvas>
+  );
+};
+{
+  /* <Canvas colorManagement camera={{ position: [0, 0, 300] }}></Canvas> */
+}
+
+export default ThreeJsTutorial;
