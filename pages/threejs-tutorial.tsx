4aab74b50b8afdd310dbfe5196f34b35557d341e
bryanz826 <bryanz826@gmail.com>
2021-03-25 13:14:58 -0700

    Reformat tsx to new Prettier config

--- pages/threejs-tutorial.tsx
+++ pages/threejs-tutorial.tsx
@@ -1,17 +1,16 @@
-import React, { useRef, useState } from "react";
-import { Canvas, useFrame } from "react-three-fiber";
-import { Mesh } from "three";
-import styles from "../styles/ThreeJsTutorial.module.scss";
+import React, { useRef, useState } from "react"
+import { Canvas, useFrame } from "react-three-fiber"
+import { Mesh } from "three"
 
 const Box = (props) => {
-  const mesh = useRef<Mesh>();
+  const mesh = useRef<Mesh>()
 
-  const [hovered, setHover] = useState(false);
-  const [active, setActive] = useState(false);
+  const [hovered, setHover] = useState(false)
+  const [active, setActive] = useState(false)
 
   useFrame(() => {
-    mesh.current.rotation.x = mesh.current.rotation.y += 0.01;
-  });
+    mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+  })
 
   return (
     <mesh
@@ -25,22 +24,22 @@ const Box = (props) => {
       <boxBufferGeometry args={[1, 1, 1]} />
       <meshStandardMaterial color={hovered ? "hotpink" : "orange"} />
     </mesh>
-  );
-};
+  )
+}
 
 const ThreeJsTutorial = () => {
   return (
-    <Canvas className={styles.container}>
+    <Canvas className="container_full-page">
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
       <pointLight position={[-10, -10, -10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
     </Canvas>
-  );
-};
+  )
+}
 {
   /* <Canvas colorManagement camera={{ position: [0, 0, 300] }}></Canvas> */
 }
 
-export default ThreeJsTutorial;
+export default ThreeJsTutorial
