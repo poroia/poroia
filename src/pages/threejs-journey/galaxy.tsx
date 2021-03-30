18ae1f155c88dd8579a8d2ff4cdca50e33cf4017
bryanz826 <bryanz826@gmail.com>
2021-03-30 00:55:32 -0700

    Fix auto-resize galaxy

--- src/pages/threejs-journey/galaxy.tsx
+++ src/pages/threejs-journey/galaxy.tsx
@@ -1,7 +1,7 @@
 import GalaxyGenerator from "../../modules/ThreeJsJourney/GalaxyGenerator"
 
 const Galaxy = () => {
-  return <GalaxyGenerator className="container_full-page" />
+  return <GalaxyGenerator className="full-page" />
 }
 
 export default Galaxy
