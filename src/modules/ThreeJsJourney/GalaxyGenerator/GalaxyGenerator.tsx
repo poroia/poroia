98ddf4094f7d2ecc9fca78d2fa98bb2568820e32
bryanz826 <bryanz826@gmail.com>
2021-03-30 01:27:24 -0700

    Better galaxy steps & defaults

--- src/modules/ThreeJsJourney/GalaxyGenerator/GalaxyGenerator.tsx
+++ src/modules/ThreeJsJourney/GalaxyGenerator/GalaxyGenerator.tsx
@@ -11,28 +11,26 @@ export const GalaxyGenerator = (props: HTMLProps<HTMLElement>) => {
   // prettier-ignore
   const [tweaks] = useTweakpane(
     {
-      count: { value: 100000, min: 100, max: 1000000, step: 100, onEventType: "finishChange", label: "count (laggy)" },
+      count: { value: 100000, min: 10000, max: 1000000, step: 10000, onEventType: "finishChange", label: "count (laggy)" },
       size: { value: 0.01, min: 0.001, max: 0.05, step: 0.0001, onEventType: "finishChange", label: "size (laggy)" },
       radius: { value: 5, min: 0.01, max: 20, step: 0.01, onEventType: "finishChange" },
       branches: { value: 3, min: 2, max: 20, step: 1, onEventType: "finishChange" },
-      spin: { value: 1, min: -5, max: 5, step: 0.001, onEventType: "finishChange" },
-      randomness: { value: 0.5, min: 0, max: 2, onEventType: "finishChange" },
-      randomnessBranchPower: { value: 3, min: 1, max: 10, step: 0.001, onEventType: "finishChange" },
-      randomnessRadiusPower: { value: 1.5, min: 1, max: 10, step: 0.001, onEventType: "finishChange" },
+      spin: { value: 1, min: -5, max: 5, step: 0.01, onEventType: "finishChange" },
+      randomness: { value: 0.5, min: 0, max: 2, step: 0.01, onEventType: "finishChange" },
+      randomnessBranchPower: { value: 3, min: 1, max: 10, step: 0.01, onEventType: "finishChange" },
+      randomnessRadiusPower: { value: 1.5, min: 1, max: 10, step: 0.01, onEventType: "finishChange" },
       insideColor: "#ff6030",
       outsideColor: "#1b3984",
     },
     { title: "Customization", expanded: false, container: debugContainer }
   )
 
-  // pane.
-
   return (
     <>
       <Canvas
         concurrent
         gl={{ antialias: false }}
-        camera={{ position: [3, 3, 3], fov: 75, near: 0.1, far: 100 }}
+        camera={{ position: [2.75, 2.75, 2.75], fov: 75, near: 0.1, far: 100 }}
         colorManagement={false}
         onCreated={({ gl }) => {
           gl.setClearColor("black")
